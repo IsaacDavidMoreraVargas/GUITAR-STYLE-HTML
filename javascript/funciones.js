3c96d4ee32029ad22ca4fbd1798b468e63fdd13c
unknown <isaacmorerawrath@hotmail.com>
2020-11-08 16:03:22 -0600

    first commit

---
+++ javascript/funciones.js
@@ -0,0 +1,9 @@
+muestra = function() {
+
+	if(document.getElementById('mostrar').style.display == 'block') {
+		document.getElementById('mostrar').style.display = 'none';
+	} else {
+		document.getElementById('mostrar').style.display = 'block';
+	}
+}
+
